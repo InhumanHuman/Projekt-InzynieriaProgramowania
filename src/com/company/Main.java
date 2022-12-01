3ce3ba510ca8b69466bc34ef795156e2f9955966
RefubuS <refubuspl@gmail.com>
2022-12-01 21:51:40 +0100

    Commit poczatkowy, dodano do main'a czerwona scene.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.RED);
+        stage.setTitle("Test");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Heja Filipo lecymy");
+        launch();
     }
 }
